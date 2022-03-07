8a389791c9a11060b0aa56b8ef67e9b1f2b44774
omonovotabek <omonovotabek9541537@gmail.com>
2022-03-08 03:21:54 +0500

    08.03.2022

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,5 +1,4 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'client',
     htmlAttrs: {
@@ -16,36 +15,28 @@ export default {
     ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    'element-ui/lib/theme-chalk/index.css'
+    'element-ui/lib/theme-chalk/index.css',
+    '@/theme/index.css'
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '@/plugins/element-ui'
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-  ],
+  ], 
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'http://localhost:5000',
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: [/^element-ui/],
   }
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <Tutorial/>
+  <el-row type="flex" justify="center">
+    <el-col :xs="24" :sm="18" :md="12" :lg="10">
+      <app-post v-for="post in 3" :key="post" />
+    </el-col>
+  </el-row>
 </template>
 
 <script>
+import AppPost from "@/components/main/Post";
 export default {
-  name: 'IndexPage'
-}
+  head:{
+    title: "Главная"
+  },
+  components: {
+    AppPost,
+  },
+};
 </script>
